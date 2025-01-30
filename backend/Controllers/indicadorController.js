d46f69501bc65b60aae2c3841072b725c7a9fc00
Deynner Alexander Sanabria Rojas <107587926+DeynnerASR@users.noreply.github.com>
2025-01-30 11:01:26 -0500

    Create indicadorController.js

---
+++ backend/Controllers/indicadorController.js
@@ -0,0 +1,31 @@
+const Indicador = require('../Models/modeloIndicador')
+
+
+const obtnerInformacion = async (req,res) =>{
+    try {
+        const {mes,annio} = req.body;
+
+        const resultado = await Indicador.realizarConsulta();
+        console.log(resultado)
+        res.status(200).json(resultado);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error al realizar las queries' });
+    }
+
+}
+
+const obtenerReporte = async (req,res)=>{
+   const {mes,annio} = req.body;
+  try{
+    const resultado = await Indicador.realizarReporte(mes,annio);
+    console.log(resultado)
+    res.status(200).json(resultado);
+  }catch(error){
+        console.log(error);
+        res.status(500).json({ error: 'Error al realizar el reporte' });
+  }
+       
+}
+
+
