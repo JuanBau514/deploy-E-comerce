f357e4bf48d0f21b208a05c75bc6b8d27dbf956a
Deynner Alexander Sanabria Rojas <107587926+DeynnerASR@users.noreply.github.com>
2025-01-30 10:57:20 -0500

    Create modeloIndicador.js

---
+++ backend/Models/modeloIndicador.js
@@ -0,0 +1,35 @@
+const db = require('./conection');
+
+
+class Indicador{
+    constructor(numeroProductos, numeroClientes, numeroAdministradores,pedidosRealizados){
+        this.numeroProductos = numeroProductos;
+        this.numeroClientes = numeroClientes;
+        this.numeroAdministradores = numeroAdministradores;
+        this.pedidosRealizados = pedidosRealizados;
+    }
+
+    static async realizarConsulta (){
+       // const query = 'SELECT COUNT(*) FROM producto';
+        const queries = {
+            "producto": "SELECT COUNT(*) FROM producto;",
+            "cliente": "SELECT COUNT(*) FROM usuario WHERE id_rol = 2;",
+            "administrador": "SELECT COUNT(*) FROM usuario WHERE id_rol=1;",
+            "pedido": "SELECT COUNT(*) FROM factura;"
+        }
+        const resultados = {}; //En este objeto, se guardaran los resultados de cada consulta.
+        for (const consulta in queries) {
+            console.log(`consulta: ${consulta}`)
+            console.log(`Haiendo la consulta: ${queries[consulta]}`)
+            
+            resultados.consulta = await db.query(queries[consulta]);
+            
+            console.log(`Resultado de la consulta: ${resultados}`)
+            
+        }
+
+        return resultados;
+    }
+}
+
+module.exports = Indicador;
