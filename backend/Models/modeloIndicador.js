cc6846884069b36a484fd564c4930e790beb0151
Deynner Alexander Sanabria Rojas <107587926+DeynnerASR@users.noreply.github.com>
2025-02-01 15:41:05 -0500

    Update modeloIndicador.js

--- backend/Models/modeloIndicador.js
+++ backend/Models/modeloIndicador.js
@@ -24,7 +24,7 @@ class Indicador{
             
             resultados.consulta = await db.query(queries[consulta]);
             
-            console.log(`Resultado de la consulta: ${resultados}`)
+            console.log(`Resultado de la consulta: ${resultados.consulta}`)
             
         }
 
