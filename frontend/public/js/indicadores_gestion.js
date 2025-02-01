6127f3acffc61977a83c27778bf110bc9d03ed55
Deynner Alexander Sanabria Rojas <107587926+DeynnerASR@users.noreply.github.com>
2025-02-01 15:19:52 -0500

    Update indicadores_gestion.js

--- frontend/public/js/indicadores_gestion.js
+++ frontend/public/js/indicadores_gestion.js
@@ -14,8 +14,9 @@ panel_cantidad_administradores.textContent = 'Prueba';
 
 const agregar_datos = async ()=>{
   try{
-     const informacion = await fetch('https://deploy-e-comerce-production.up.railway.app/api/users/indicadorGeneral');   
-    console.log(informacion.json());
+     const respuesta = await fetch('https://deploy-e-comerce-production.up.railway.app/api/users/indicadorGeneral');   
+     const informacion.json();
+    console.log(informacion);
   }
   catch(error){
     console.log(error);
