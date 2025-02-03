d50a53428b0a330a9fa4b667d3de7b1b647c862a
Deynner Alexander Sanabria Rojas <107587926+DeynnerASR@users.noreply.github.com>
2025-02-03 13:25:01 -0500

    Update indicadorController.js

--- backend/Controllers/indicadorController.js
+++ backend/Controllers/indicadorController.js
@@ -2,10 +2,15 @@ import  Indicador from '../Models/modeloIndicador.js';
 
 async function obtenerInformacion (req,res) {
     const queries =  ['SELECT COUNT(*) FROM producto;',"SELECT COUNT(*) FROM usuario WHERE id_rol = 2;","SELECT COUNT(*) FROM usuario WHERE id_rol=1;","SELECT COUNT(*) FROM factura;"] 
+    const resultados = []
     try {
-        const resultado = await Indicador.realizarConsulta('SELECT * FROM producto;');
-        console.log(resultado)
-        res.status(200).json(resultado);
+        queries.forEach((consulta)=>{
+        const consulta = Indicador.realizarConsulta('SELECT * FROM producto;');    
+        resultados.push(resultado);
+        })
+        
+        console.log(resultados)
+        res.status(200).json(resultados);
     } catch (error) {
         console.log(error);
         res.status(500).json({ error:`Error al realizar las queries ${error}` });
