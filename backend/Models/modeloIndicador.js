1e6dcc6764719d8237d8a54abce7506c5dad074b
Deynner Alexander Sanabria Rojas <107587926+DeynnerASR@users.noreply.github.com>
2025-02-04 14:02:43 -0500

    Update modeloIndicador.js

--- backend/Models/modeloIndicador.js
+++ backend/Models/modeloIndicador.js
@@ -2,56 +2,9 @@ import db from './conection.js';
 
 
 class Indicador{
-    constructor(numeroProductos, numeroClientes, numeroAdministradores,pedidosRealizados){
-        this.numeroProductos = numeroProductos;
-        this.numeroClientes = numeroClientes;
-        this.numeroAdministradores = numeroAdministradores;
-        this.pedidosRealizados = pedidosRealizados;
-    }
-
-
-    static async realizarConsulta (){
-    
-       // const query = 'SELECT COUNT(*) FROM producto'
-
-        db.query(`
-        SELECT COUNT(*) FROM producto;
-        SELECT COUNT(*) FROM usuario WHERE id_rol = 2;
-        SELECT COUNT(*) FROM usuario WHERE id_rol=1;
-        SELECT COUNT(*) FROM factura;
-        `,function (error, resultados, fields) {
-          if (error) throw error;
-            
-          return resultados;
-        }
-        );
-      
-    }
-
-    static async realizarReporte (mes,annio)
-    {
-        const consultas = 
-        {
-            "Cantidad_usuarios": "Select count(*) from usuario",
-            "Gente": "SELECT * FROM usuario",
-            "cantidad_productos": "Select count(*) from producto",
-            "productos": "Select * from producto",
-        }
-
-        const resultados = {}
-
-        for( const consulta in consultas){
-             resultados.consulta = await db.query(consultas[consulta]);            
-        }
-
-        for( const resultado in resultados)
-        {
-            console.log(`Resultado de la consulta:  ${resultados[resultado]}`)
-        }
-
-        return resultados;
-    }
-    
+    static async realizarConsulta (consulta){
+        return await db.query(consulta);
+    }    
 }
 //
 //export.default Indicador;
